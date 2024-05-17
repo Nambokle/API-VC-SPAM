23736025221b2bd7ec0f786ca06fd6d4058b1e4f
Nambokle <135355990+Nambokle@users.noreply.github.com>
2024-05-18 01:33:14 +0700

    Update and rename README.md to Discord-API.py

---
+++ Discord-API.py
@@ -0,0 +1,11 @@
+executor = ThreadPoolExecutor(max_workers=int(1000))
+def run(token):
+  while True:
+    ws = WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=8&encoding=json")
+    hello = loads(ws.recv())
+    heartbeat_interval = hello['d']['heartbeat_interval']
+    ws.send(dumps({"op": 2,"d": {"token": token,"properties": {"$os": "windows","$browser": "Discord","$device": "desktop"}}}))
+    ws.send(dumps({"op": 4,"d": {"guild_id": server,"channel_id": channel,"self_mute": mute,"self_deaf": deaf, "self_stream?": stream, "self_video": video}}))
+    ws.send(dumps({"op": 18,"d": {"type": "guild","guild_id": server,"channel_id": channel,"preferred_region": "singapore"}}))
+    ws.send(dumps({"op": 1,"d": None}))
--- README.md
+++
@@ -1,1 +0,0 @@
-# API-VC-SPAM
